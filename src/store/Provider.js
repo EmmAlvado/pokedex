a9f6994ce2f06393d99f67c7071e4aa583ee60f3
emma.alvado <emma.alvado@mirakl.com>
2019-05-21 21:56:34 +0200

    Typping with flow

--- src/store/Provider.js
+++ src/store/Provider.js
@@ -5,43 +5,35 @@ import { Provider as ReduxProvider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 
-import {
-    routerReducer as routing,
-    routerMiddleware,
-} from 'react-router-redux';
+import { routerReducer as routing, routerMiddleware } from 'react-router-redux';
 
-import { createMemoryHistory } from 'history'
-
+import { createMemoryHistory } from 'history';
 
 const rootReducer = combineReducers({
     form: formReducer,
     routing,
-})
+});
 
 const history = createMemoryHistory();
 
 const middleware = routerMiddleware(history);
 
-let currentWindow = {};
+const currentWindow = {};
 
 export type Props = {
-    children: Node
+    children: Node,
 };
 
 const store = createStore(
     rootReducer,
     currentWindow &&
-    currentWindow.__REDUX_DEVTOOLS_EXTENSION__ &&
-    currentWindow.__REDUX_DEVTOOLS_EXTENSION__(),
+        currentWindow.__REDUX_DEVTOOLS_EXTENSION__ &&
+        currentWindow.__REDUX_DEVTOOLS_EXTENSION__(),
     applyMiddleware(middleware)
 );
 
-function Provider({ children }:Props) {
-    return (
-        <ReduxProvider store={store}>
-                { children }
-        </ReduxProvider>
-    );
+function Provider({ children }: Props) {
+    return <ReduxProvider store={store}>{children}</ReduxProvider>;
 }
 
 Provider.store = store;
